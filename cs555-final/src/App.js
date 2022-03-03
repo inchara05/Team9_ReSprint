612d6f43028e57aeae1eb1beff7ee87783caeee0
Pranes Gopalan Venkatesh <pranesgopalanvenkatesh@praness-mbp.myfiosgateway.com>
2022-03-02 19:28:01 -0500

    Tap to start button

--- cs555-final/src/App.js
+++ cs555-final/src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
 
+function startClick() {
+  alert("Tap to start works");
+}
+
 function App() {
   return (
+    <>
     <div className="App">
       Welcome to the Balloon Burst Game!
     </div>
+    <div>
+        <button onClick={startClick}>
+          Tap to start
+        </button>
+      </div></>
   );
 }
+
 
 export default App;
