001049c4fbd720766cf7cd6943a8de66c30196a0
Jeshwanth Kumar <jkumar3@stevens.edu>
2022-03-24 22:00:54 -0400

    Test File

--- cs555-final/src/App.test.js
+++ cs555-final/src/App.test.js
@@ -1,8 +1,19 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import renderer from "react-test-renderer";
+import { Link } from "react-router-dom";
+import { BrowserRouter } from "react-router";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+  const component = renderer.create(
+    <BrowserRouter location="/game" context={context}>
+      <Link to="/game" />
+    </BrowserRouter>
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapShot();
 });
