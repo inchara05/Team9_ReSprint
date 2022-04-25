bf9052b0b4298e1d7307e0ccf2fe7d507b7c5a66
Ar Koushal <arkoushal1999@gmail.com>
2022-04-24 22:17:15 -0400

    Modified Game component

--- cs555-final/src/components/GameComponents/Game.js
+++ cs555-final/src/components/GameComponents/Game.js
@@ -2,11 +2,18 @@ import React, { useState, useEffect } from "react";
 import { Grid, Chip, Button, Stack, Typography } from "@mui/material";
 import BalloonInitialImage from "../../images/balloon.png";
 import BalloonBurstImage from "../../images/burst.png";
+import start from "../../audioClips/start.mp3";
 import Session from "./Session";
+import GameOver from "./GameOver";
+import hintImage from "../../images/bulb.png";
 const axios = require("axios");
+const uuid = require("uuid");
+
+const startAudio = new Audio(start);
 
 const Game = () => {
   const [totalScore, setTotalScore] = useState(0);
+  const [sessionId, setSessionId] = useState(uuid.v4());
   const [sequenceHint, setSequenceHint] = useState("");
   let clicks = 0;
   const [balloons, setBalloons] = useState([]);
@@ -29,10 +36,41 @@ const Game = () => {
     fetchSequence();
   }, []);
 
+  const handleHint = () => {
+    console.log(sequenceHint);
+    alert(sequenceHint);
+  };
+
+  useEffect(() => {
+    // handle when game gets over
+  }, [gameOver]);
+
   return (
-    <div>
-      {balloons.length > 0 && !gameOver && <Session clicks={clicks} balloons={balloons} setGameOver={setGameOver} />}
-      {gameOver && <h1>Game Over!</h1>}
+    <div
+      onLoad={() => {
+        startAudio.play();
+      }}
+    >
+      {balloons.length > 0 && !gameOver && (
+        <Session
+          clicks={clicks}
+          totalScore={totalScore}
+          setTotalScore={setTotalScore}
+          balloons={balloons}
+          setGameOver={setGameOver}
+        />
+      )}
+      <Button
+        className="hintButton"
+        style={{ width: "25%" }}
+        label="Hint"
+        onClick={() => {
+          handleHint();
+        }}
+      >
+        <img alt="a balloon to play with" style={{ padding: "4%", width: "5%" }} src={hintImage}></img>
+      </Button>
+      {gameOver && <GameOver sessionId={sessionId} totalScore={totalScore} />}
     </div>
   );
 };
