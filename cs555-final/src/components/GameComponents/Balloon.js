8d6e7b7ce30c67149e330c63f2b6570e267a2000
Koushik Raja <koushikraja@koushiks-mbp.myfiosgateway.com>
2022-04-24 22:48:23 -0400

    Modified Balloon component

--- cs555-final/src/components/GameComponents/Balloon.js
+++ cs555-final/src/components/GameComponents/Balloon.js
@@ -2,37 +2,40 @@ import React, { useState, useEffect } from "react";
 import { Grid, Chip, Button, Stack, Typography } from "@mui/material";
 import BalloonInitialImage from "../../images/balloon.png";
 import BalloonBurstImage from "../../images/burst.png";
+import audio1 from "../../audioClips/1.mpeg";
 const GROWTH_FACTOR = 0.02;
 
-const Balloon = ({ isBalloonAlive, handleBalloonClick }) => {
-  // const [width, setWidth] = useState(initWidth);
-  // console.log("Width :", width);
-  let width = "5%";
-  //   const handleBalloonPump = () => {
-  //     let currentWidth = width.substring(0, width.length - 1);
-  //     let currentWidthInDecimal = parseInt(currentWidth) / 100;
-  //     let settingWidthInDecimal = currentWidthInDecimal + GROWTH_FACTOR;
-  //     let settingWidth = settingWidthInDecimal * 100 + "%";
-  //     setWidth(settingWidth);
-  //   };
+const Balloon = ({ initWidth, isBalloonAlive, handleBalloonClick, shouldResetGame }) => {
+  const [balloonWidth, setBalloonWidth] = useState(initWidth);
+  const audio = new Audio(audio1);
+  useEffect(() => {
+    if (!isBalloonAlive) setBalloonWidth(50);
+    if (shouldResetGame) setBalloonWidth(50);
+  }, [isBalloonAlive, shouldResetGame]);
 
+  const increaseBalloonSize = () => {
+    setBalloonWidth(balloonWidth + 50);
+  };
   return (
     <div>
       <Grid style={{ position: "fixed" }} container alignItems={"center"} justifyContent={"center"}>
         {isBalloonAlive && (
           <img
             alt="a balloon to play with"
-            style={{ padding: "4%", width: width }}
+            style={{ padding: "4%", width: balloonWidth }}
             src={BalloonInitialImage}
+            onMouseDown={() => {
+              audio.play();
+            }}
             onClick={() => {
               handleBalloonClick();
-              // handleBalloonPump();
+              increaseBalloonSize();
             }}
           />
         )}
         {!isBalloonAlive && (
           <img
-            style={{ padding: "13%", width: width, maxWidth: width }}
+            style={{ padding: "13%", width: balloonWidth, maxWidth: balloonWidth }}
             alt="a balloon that has burst"
             src={BalloonBurstImage}
           />
